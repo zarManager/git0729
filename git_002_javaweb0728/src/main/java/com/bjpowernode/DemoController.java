53eb9bf137809277ee20c489e8802f8352b7602a
Administrator <zarManager@qq.com>
2022-07-29 17:13:55 +0800

    项目经理在程序员克隆完项目后又更新的内容

--- git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
+++ git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
@@ -6,5 +6,6 @@ package com.bjpowernode;
 public class DemoController {
     public static void main(String[] args) {
         System.out.println("这是javaWeb的项目................");
+        System.out.println("项目经理在程序员克隆完项目后又更新.........");
     }
 }
