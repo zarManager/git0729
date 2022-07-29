8022b09d869a5a1bd1ac5472b70d749131394ec9
Administrator <zarManager@qq.com>
2022-07-29 16:03:45 +0800

    再次提交新建项目

---
+++ git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
@@ -0,0 +1,10 @@
+package com.bjpowernode;
+
+/**
+ *
+ */
+public class DemoController {
+    public static void main(String[] args) {
+        System.out.println("这是javaWeb的项目................");
+    }
+}
