dc4e4ebbe61df577859f4ef2e12fab53b1fe60f8
Administrator <zarManager@qq.com>
2022-07-29 16:24:02 +0800

    在HelloJavaSE类中增加内容

--- git_001_javase0728/src/main/java/com/bjpowernode/HelloJavaSE.java
+++ git_001_javase0728/src/main/java/com/bjpowernode/HelloJavaSE.java
@@ -6,5 +6,6 @@ package com.bjpowernode;
 public class HelloJavaSE {
     public static void main(String[] args) {
         System.out.println("这是javaSE的项目...........");
+        System.out.println("更新的项目的内容.........");
     }
 }
