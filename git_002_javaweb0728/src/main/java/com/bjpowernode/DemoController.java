53a716c656b28328340c71c933baedceb305e245
Administrator <zarManager@qq.com>
2022-07-30 09:03:42 +0800

    程序员在DemoController中增加了新的内容

--- git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
+++ git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
@@ -7,5 +7,6 @@ public class DemoController {
     public static void main(String[] args) {
         System.out.println("这是javaWeb的项目................");
         System.out.println("项目经理在程序员克隆完项目后又更新.........");
+        System.out.println("程序员zarcode开发的内容.........");
     }
 }
