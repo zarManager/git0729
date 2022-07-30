9e6ebe7d390f25b1f8f80fc32b20c43a0d953cab
Administrator <zarManager@qq.com>
2022-07-30 09:15:16 +0800

    程序员被邀请后在DemoController中增加了新的内容

--- git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
+++ git_002_javaweb0728/src/main/java/com/bjpowernode/DemoController.java
@@ -8,5 +8,6 @@ public class DemoController {
         System.out.println("这是javaWeb的项目................");
         System.out.println("项目经理在程序员克隆完项目后又更新.........");
         System.out.println("程序员zarcode开发的内容.........");
+        System.out.println("程序员被邀请后再次开发的内容...........");
     }
 }
